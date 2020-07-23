5b98ea624239225d950a1d7e0725aeb0c4d4cc77
danitw <twz3xc21@gmail.com>
2020-07-23 11:57:57 -0300

    url amigavel +- feita

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
 
 session_start();
 
-/* Deixa isso aq pq eu tenho q olhar dps 
-if ($_SESSION['logado'] === true) {
-	require_once './views/index.php';
-	echo "a";
-} elseif (isset($_SESSION['logado']) || $_SESSION['logado'] === false) {
-	require_once './views/no-user-home.php';
-	echo 'dksal';
-} */
-
 if ($_SESSION['logado'] === true) {
 	require_once './views/index.php';
 } else {
