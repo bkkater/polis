450159c1c6ac9efce12242d5afc51cfeba039746
danitw <twz3xc21@gmail.com>
2020-07-24 15:03:18 -0300

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel adsdadsadas
                 </div>
 
                 <div class="links">
